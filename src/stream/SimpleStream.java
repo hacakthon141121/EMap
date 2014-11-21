ae86dd4166876b64f1bed4bdf772e2a72e4f70aa
Yilin <Yilin@E>
2014-11-21 18:03:14 -0500

    Now can filter tweets with happy emoticons and add to database.

--- src/stream/SimpleStream.java
+++ src/stream/SimpleStream.java
@@ -10,10 +10,12 @@ import twitter4j.StatusListener;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 import twitter4j.conf.ConfigurationBuilder;
+
 import database.DatabaseOperation;
 
 public class SimpleStream {
 	static int numOfRecordsCollected = 0, limit = 100000;
+	static String[] listHappiness = {":-)", ":)", ";)", "<3", ":D", ":P", ":D", ":P", ";-)", ":-D", "XD", ":')", "=)", "C:", ":L", ":3", ";D", ":-P", ";-P", ":d", ":*", "=D", "d^_^b", ":-9", ":'-)", ":)))", "n_n", ":O)", "^_^", "=P", "(;", ":]", ";P", "/-)", ":'3", "^.^", ":}", ";-D", ";')", "=]", "C':", ";]", ":-*", ":'D", ":1", "(-;", ";L", ">:)", "X-D", "B-)", ":>", "8-D"};
 	
     public static void main(String[] args) {
         ConfigurationBuilder cb = new ConfigurationBuilder();
@@ -58,14 +60,21 @@ public class SimpleStream {
                 GeoLocation location = status.getGeoLocation();
                 double xlabel = location.getLatitude();
                 double ylabel = location.getLongitude();
-                String emoticon = "TODO";
+                String emoticon = null;
                 
                 // Store current tweet into database
-//                System.out.println(content);///
-//                System.out.println(xlabel);///
-//                System.out.println(ylabel);///
+                System.out.println(content);///
+                System.out.println(xlabel);///
+                System.out.println(ylabel);///
                 try {
-        			db.addRowToTable("happiness", content, xlabel, ylabel, emoticon);
+                	for(int i = 0; i < listHappiness.length; i++){
+                		if(content.toLowerCase().contains(listHappiness[i].toLowerCase())){
+                			emoticon = listHappiness[i];
+                			System.out.println("===emoticon is '" + emoticon + "'");///
+                			db.addRowToTable("happiness", content, xlabel, ylabel, emoticon);
+                			break;
+                		}
+                	}
         			numOfRecordsCollected++;
         		} catch (Exception e) {
         			// TODO: handle exception
@@ -85,7 +94,7 @@ public class SimpleStream {
         
         FilterQuery fq = new FilterQuery();
     
-        String keywords[] = {"food", "cloud", "computing", "homework", "time", "consuming"};
+        String keywords[] = listHappiness;
 
         fq.track(keywords);
 
